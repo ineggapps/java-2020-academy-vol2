81f51d320e9b9c957797d3ac5d62805dccd24546
inegg <inegg.apps@gmail.com>
2020-03-30 16:57:10 +0900

    Connection 객체를 통한 Select문 실행 후 결과 반환받기 (복수 개의 결과 출력)

---
+++ src/d200330/Test4SelectQuery.java
@@ -0,0 +1,51 @@
+package d200330;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.util.DBConn;
+
+public class Test4SelectQuery {
+	public static void main(String[] args) {
+		Connection conn = DBConn.getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;
+		String sql;
+		try {
+			stmt = conn.createStatement();
+			sql = "SELECT name, TO_CHAR(birth,'YYYY-MM-DD') birth, kor, eng, mat, kor+eng+mat tot, hak FROM score";
+//			sql = "SELECT name, birth, kor, eng, mat, kor+eng+mat tot FROM score";
+			rs = stmt.executeQuery(sql);
+
+			while(rs.next()) {//결괏값 복수 개 출력하기
+				String hak = rs.getString("hak");
+				String name = rs.getString("name");//또는 rs.getString(1);
+				String birth = rs.getString("birth");//날짜이지만 String형으로도 변환되어 저장이 가능하다. rs.getString(2);
+				int kor = rs.getInt("kor");
+				int eng = rs.getInt("eng");
+				int mat = rs.getInt("mat");
+				int tot = rs.getInt("tot");
+				System.out.println(String.format("%s, %s, %s, %d, %d, %d, %d",hak, name,birth,kor,eng,mat,tot));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (Exception e) {
+			}
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+			} catch (Exception e) {
+			}
+		}
+	}
+}
