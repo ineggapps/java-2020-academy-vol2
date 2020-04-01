7cf8c4fa03d4080a7e276506df33d4ba207796a8
inegg <inegg.apps@gmail.com>
2020-04-01 12:49:58 +0900

    급여관리 기능 완성

--- src/db/employee/Salary.java
+++ src/db/employee/Salary.java
@@ -1,67 +1,227 @@
 package db.employee;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Salary {
-	private Scanner sc=new Scanner(System.in);
-	private SalaryDAO dao=new SalaryDAO();
-	private Employee emp=null;
-	
+	private Scanner sc = new Scanner(System.in);
+	private SalaryDAO dao = new SalaryDAO();
+	private Employee emp = null;
+
 	public Salary(Employee emp) {
 		this.emp = emp;
 	}
-	
+
 	public void salaryManage() {
 		int ch;
-		while(true) {
+		while (true) {
 			System.out.println("\n[급여관리]");
 			do {
 				System.out.print("1.지급 2.수정 3.삭제 4.월별리스트 5.사번검색 6.리스트 7.사원리스트 8.메인 => ");
 				ch = sc.nextInt();
-			}while(ch<1||ch>8);
-			
-			if(ch==8) return;
-			
-			switch(ch) {
-			case 1:payment(); break;
-			case 2:update(); break;
-			case 3:delete(); break;
-			case 4:monthList(); break;
-			case 5:searchSabeon(); break;
-			case 6:list(); break;
-			case 7:emp.list(); break;
+			} while (ch < 1 || ch > 8);
+
+			if (ch == 8)
+				return;
+
+			switch (ch) {
+			case 1:
+				payment();
+				break;
+			case 2:
+				update();
+				break;
+			case 3:
+				delete();
+				break;
+			case 4:
+				monthList();
+				break;
+			case 5:
+				searchSabeon();
+				break;
+			case 6:
+				list();
+				break;
+			case 7:
+				emp.list();
+				break;
 			}
 		}
 	}
-	
+
 	public void payment() {
 		System.out.println("\n급여 지급...");
-		
+
+		try {
+			SalaryDTO dto = new SalaryDTO();
+			System.out.print("사원번호 입력 > ");
+			dto.setSabeon(sc.next());
+
+			System.out.print("급여년월 입력 > ");
+			dto.setPayDate(sc.next());
+
+			System.out.print("금여지급일자입력 > ");
+			dto.setPaymentDate(sc.next());
+
+			System.out.print("기본급 입력 > ");
+			dto.setPay(sc.nextInt());
+
+			System.out.print("수당 입력 > ");
+			dto.setSudang(sc.nextInt());
+
+			// 세금 계산
+			int total = dto.getPay() + dto.getSudang();
+			dto.setTax(calculateTax(total));
+
+			System.out.print("메모사항 입력 > ");
+			dto.setMemo(sc.next());
+
+			int result = dao.insertSalary(dto);
+			if (result >= 1) {
+				System.out.println("급여 사항이 입력되었습니다.");
+			} else {
+				System.out.println("[실패] 급여 사항이 입력되지 않았습니다.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	public void update() {
 		System.out.println("\n급여 수정...");
-		
+		try {
+
+			System.out.print("수정할 급여번호 > ");
+			int salaryNum = sc.nextInt();
+			SalaryDTO dto = dao.readSalary(salaryNum);
+			if (dto == null) {
+				System.out.println("급여번호를 잘못 입력하셨습니다.");
+				return;
+			}
+			printDTO(dto);
+
+			System.out.print("수정할 급여년월 > ");
+			dto.setPayDate(sc.next());
+
+			System.out.print("수정할 급여지급일자 > ");
+			dto.setPaymentDate(sc.next());
+
+			System.out.print("수정할 기본급 > ");
+			dto.setPay(sc.nextInt());
+
+			System.out.print("수정할 수당 > ");
+			dto.setSudang(sc.nextInt());
+
+			int total = dto.getPay() + dto.getSudang();
+			dto.setTax(calculateTax(total));
+
+			System.out.print("수정할 메모> ");
+			dto.setMemo(sc.next());
+
+			int result = dao.updateSalary(dto);
+			if (result >= 1) {
+				System.out.println("급여 정보가 수정되었습니다.");
+			} else {
+				System.out.println("[실패] 급여정보 수정 실패");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private int calculateTax(int total) {
+		int tax = 0;
+		if (total >= 3000000) {
+			tax = (int) (total * 0.03);
+		} else if (total >= 2000000) {
+			tax = (int) (total * 0.02);
+		}
+		return tax;
 	}
 
 	public void delete() {
 		System.out.println("\n급여 삭제...");
-		
+		try {
+			System.out.print("삭제할 급여번호 > ");
+			int salaryNum = sc.nextInt();
+			int result = dao.deleteSalary(salaryNum);
+			if (result >= 1) {
+				System.out.println("해당 급여내역이 삭제되었습니다");
+			} else {
+				System.out.println("[실패] 급여내역 번호를 다시 한 번 더 확인해 주세요.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public void searchSabeon() {
 		System.out.println("\n사번 검색...");
-		
-		
+		List<SalaryDTO> list;
+		String sabeon;
+		String payDate;
+		try {
+			System.out.print("사원번호 입력 > ");
+			sabeon = sc.next();
+			System.out.print("급여년월 입력 > ");
+			payDate = sc.next();
+
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put(SalaryDAO.INPUT_SABEON, sabeon);
+			map.put(SalaryDAO.INPUT_PAYDATE, payDate);
+
+			list = dao.listSalary(map);
+			printAllDTO(list);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void monthList() {
 		System.out.println("\n월별 리스트...");
-		
+		try {
+			System.out.print("급여년월 입력 > ");
+			String payDate = sc.next();
+			printAllDTO(dao.listSalary(payDate));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void list() {
 		System.out.println("\n급여 리스트...");
-	
+		printAllDTO(dao.listSalary());
 	}
+
+	private void printAllDTO(List<SalaryDTO> list) {
+		for (SalaryDTO dto : list) {
+			printDTO(dto);
+		}
+	}
+
+	private void printDTO(SalaryDTO dto) {
+		if (dto == null) {
+			System.out.println("DTO is NULL...");
+			return;
+		}
+		System.out.print("[");
+		System.out.print(dto.getSalaryNum() + ", ");
+		System.out.print(dto.getSabeon() + ", ");
+		System.out.print(dto.getPayDate() + ", ");
+		System.out.print(dto.getPaymentDate() + ", ");
+		System.out.print(dto.getPay() + ", ");
+		System.out.print(dto.getSudang() + ", ");
+		System.out.print(dto.getTax() + ", ");
+		System.out.print(dto.getTot() + ", ");
+		System.out.print(dto.getAfterPay() + ", ");
+		System.out.print(dto.getMemo() + "]");
+		System.out.println();
+	}
+
 }
--- src/db/employee/SalaryDAO.java
+++ src/db/employee/SalaryDAO.java
@@ -1,54 +1,281 @@
 package db.employee;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import com.util.DBConn;
 
+/*
+ * desc salary
+이름          널?       유형             
+----------- -------- -------------- 
+SALARYNUM   NOT NULL NUMBER         
+SABEON      NOT NULL VARCHAR2(30)   
+PAYDATE     NOT NULL VARCHAR2(30)   
+PAYMENTDATE NOT NULL DATE           
+PAY                  NUMBER(8)      
+SUDANG               NUMBER(8)      
+TAX                  NUMBER(8)      
+MEMO                 VARCHAR2(1000) 
+
+CREATE SEQUENCE salary_seq
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE;
+
+ * */
+
 public class SalaryDAO {
-	private Connection conn=DBConn.getConnection();
-	
+	private Connection conn = DBConn.getConnection();
+	public static final String INPUT_PAYDATE = "paydate";
+	public static final String INPUT_SABEON = "sabeon";
+
 	public int insertSalary(SalaryDTO dto) {
-		int result=0;
-		
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql = "INSERT INTO salary(salarynum, sabeon, paydate, paymentdate, pay, sudang, tax, memo) "
+				+ "VALUES(salary_seq.NEXTVAL, ?,?,TO_DATE(?,'YYYYMMDD'),?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getSabeon());
+			pstmt.setString(2, dto.getPayDate());
+			pstmt.setString(3, dto.getPaymentDate());
+			pstmt.setInt(4, dto.getPay());
+			pstmt.setInt(5, dto.getSudang());
+			pstmt.setInt(6, dto.getTax());
+			pstmt.setString(7, dto.getMemo());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
 		return result;
 	}
-	
+
 	public int updateSalary(SalaryDTO dto) {
-		int result=0;
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql = "UPDATE salary SET paydate=?, paymentdate=TO_DATE(?,'YYYYMMDD'), pay=?, sudang=?, tax=?, memo=? WHERE salarynum=?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getPayDate());
+			pstmt.setString(2, dto.getPaymentDate());
+			pstmt.setInt(3, dto.getPay());
+			pstmt.setInt(4, dto.getSudang());
+			pstmt.setInt(5, dto.getTax());
+			pstmt.setString(6, dto.getMemo());
+			pstmt.setInt(7, dto.getSalaryNum());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
 		return result;
 	}
 
 	public int deleteSalary(int salaryNum) {
-		int result=0;
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql = "DELETE FROM salary WHERE salarynum = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, salaryNum);
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
 
 		return result;
 	}
-	
+
 	public SalaryDTO readSalary(int salaryNum) {
-		SalaryDTO dto=null;
-		
+		SalaryDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT salarynum, sabeon, paydate, paymentdate, pay, sudang, tax, memo FROM salary WHERE salarynum=?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, salaryNum);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				dto = new SalaryDTO();
+				dto.setSalaryNum(rs.getInt("salarynum"));
+				dto.setSabeon(rs.getString("sabeon"));
+				dto.setPayDate(rs.getString("paydate"));
+				dto.setPaymentDate(rs.getDate("paymentdate").toString());
+				dto.setPay(rs.getInt("pay"));
+				dto.setSudang(rs.getInt("sudang"));
+				dto.setTax(rs.getInt("tax"));
+				dto.setTot(dto.getPay() + dto.getSudang());// 실급여
+				dto.setAfterPay(dto.getTot() - dto.getTax());// 세제후급여
+				dto.setMemo(rs.getString("memo"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
 		return dto;
 	}
-	
+
 	public List<SalaryDTO> listSalary(String payDate) {
-		List<SalaryDTO> list=new ArrayList<>();
-		
+		List<SalaryDTO> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT salarynum, sabeon, paydate, paymentdate, pay, sudang, tax, memo FROM salary WHERE paydate=? "
+				+ "ORDER BY sabeon, paydate desc";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, payDate);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				SalaryDTO dto = new SalaryDTO();
+				dto.setSalaryNum(rs.getInt("salarynum"));
+				dto.setSabeon(rs.getString("sabeon"));
+				dto.setPayDate(rs.getString("paydate"));
+				dto.setPaymentDate(rs.getDate("paymentdate").toString());
+				dto.setPay(rs.getInt("pay"));
+				dto.setSudang(rs.getInt("sudang"));
+				dto.setTax(rs.getInt("tax"));
+				dto.setTot(dto.getPay() + dto.getSudang());// 실급여
+				dto.setAfterPay(dto.getTot() - dto.getTax());// 세제후급여
+				dto.setMemo(rs.getString("memo"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
 		return list;
 	}
-	
+
 	public List<SalaryDTO> listSalary(Map<String, Object> map) {
-		List<SalaryDTO> list=new ArrayList<>();
-		
+		List<SalaryDTO> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT salarynum, sabeon, paydate, paymentdate, pay, sudang, tax, memo FROM salary WHERE sabeon=? and paydate=?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, (String) map.get(INPUT_SABEON));
+			pstmt.setString(2, (String) map.get(INPUT_PAYDATE));
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				SalaryDTO dto = new SalaryDTO();
+				dto.setSalaryNum(rs.getInt("salarynum"));
+				dto.setSabeon(rs.getString("sabeon"));
+				dto.setPayDate(rs.getString("paydate"));
+				dto.setPaymentDate(rs.getDate("paymentdate").toString());
+				dto.setPay(rs.getInt("pay"));
+				dto.setSudang(rs.getInt("sudang"));
+				dto.setTax(rs.getInt("tax"));
+				dto.setTot(dto.getPay() + dto.getSudang());// 실급여
+				dto.setAfterPay(dto.getTot() - dto.getTax());// 세제후급여
+				dto.setMemo(rs.getString("memo"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
 		return list;
 	}
 
 	public List<SalaryDTO> listSalary() {
-		List<SalaryDTO> list=new ArrayList<>();
-		
-		
+		List<SalaryDTO> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT salarynum, sabeon, paydate, paymentdate, pay, sudang, tax, memo FROM salary";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				SalaryDTO dto = new SalaryDTO();
+				dto.setSalaryNum(rs.getInt("salarynum"));
+				dto.setSabeon(rs.getString("sabeon"));
+				dto.setPayDate(rs.getString("paydate"));
+				dto.setPaymentDate(rs.getDate("paymentdate").toString());
+				dto.setPay(rs.getInt("pay"));
+				dto.setSudang(rs.getInt("sudang"));
+				dto.setTax(rs.getInt("tax"));
+				dto.setTot(dto.getPay() + dto.getSudang());// 실급여
+				dto.setAfterPay(dto.getTot() - dto.getTax());// 세제후급여
+				dto.setMemo(rs.getString("memo"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
 		return list;
 	}
 
