3383eb44bf0129eea33a32c49343335632a60677
inegg <inegg.apps@gmail.com>
2020-04-02 14:48:04 +0900

    트랜잭션 사용 (수동 커밋으로 데이터 추가하기) 성공 시 커밋, 실패 시 롤백, 완료 시 자동커밋으로 원복 작업이 필요하다. 오류난 경우 Exception이 아닌 SQLException에서 처리해야 하며 try-catch구문으로 감싸야 한다

---
+++ src/db/member4/Member.java
@@ -0,0 +1,175 @@
+package db.member4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Member {
+	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private MemberDAO dao = new MemberDAOImpl();
+
+	private void printTitle(String title) {
+		System.out.println("#########################");
+		System.out.println(title);
+		System.out.println("#########################");
+	}
+
+	public void insertMember() {
+		printTitle("회원 등록");
+		MemberDTO dto = new MemberDTO();
+		try {
+
+			System.out.print("신규 아이디 입력 > ");
+			String id = br.readLine();
+			dto.setId(id);
+
+			// 아이디 중복체크
+			if (dao.readMember(dto.getId()) != null) {
+				System.out.println("￣へ ￣ " + id + "는 사용하실 수 없습니다.");
+				System.out.println();
+				return;
+			}
+
+			System.out.print("이름 입력 > ");
+			dto.setName(br.readLine());
+
+			System.out.print("비밀번호 입력 > ");
+			dto.setPwd(br.readLine());
+
+			System.out.print("생년월일 입력 > ");
+			dto.setBirth(br.readLine());
+
+			System.out.print("이메일 입력 > ");
+			dto.setEmail(br.readLine());
+
+			System.out.print("전화번호 입력 > ");
+			dto.setTel(br.readLine());
+
+			int result = dao.insertMember(dto);
+//			System.out.println(result + "건 등록 완료!");
+			if(result!=0){				
+				System.out.println("○( ＾皿＾)っ 회원 등록 성공...");
+			}else {
+				System.out.println("등록 실패");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void updateMember() {
+		MemberDTO dto = null;
+		printTitle("회원 수정");
+		try {
+			System.out.print("수정할 회원 아이디 > ");
+			String id = br.readLine();
+			dto = dao.readMember(id);
+
+			if (dto == null) {
+				System.out.println("￣へ ￣ 등록된 아이디가 아닙니다.");
+				return;
+			}
+			printDTO(dto);
+
+			System.out.println("##### 변경 작업을 시작합니다.");
+			System.out.print("비밀번호 > ");
+			dto.setPwd(br.readLine());
+			System.out.print("생일 > ");
+			dto.setBirth(br.readLine());
+			System.out.print("이메일 > ");
+			dto.setEmail(br.readLine());
+			System.out.print("전화번호 > ");
+			dto.setTel(br.readLine());
+
+			int result = dao.updateMember(dto);
+			if(result!=0) {				
+				System.out.println("(^人^) 회원정보가 변경되었습니다.");
+			}else {
+				System.out.println("회원정보 변경 실패");
+			}
+		} catch (
+
+		Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void deleteMember() {
+		printTitle("회원 삭제");
+
+		try {
+			System.out.print("삭제할 아이디 입력 > ");
+			String id = br.readLine();
+			int result = dao.deleteMember(id);
+			if (result == 0) {
+				System.out.println("￣へ￣ 아이디가 존재하지 않습니다. (" + id + ")");
+				return;
+			}
+			if(result!=0) {				
+				System.out.println(id + "d=====(￣▽￣*)b 계정을 삭제하였습니다.");
+			}else {
+				System.out.println("계정 삭제 실패 X_X");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void listMember() {
+		printTitle("회원 목록 조회");
+
+		List<MemberDTO> list = dao.listMember();
+		for (MemberDTO dto : list) {
+			printDTO(dto);
+		}
+	}
+
+	public void findById() {
+		printTitle("아이디로 회원 검색");
+		try {
+			System.out.print("아이디 > ");
+			String id = br.readLine();
+			MemberDTO dto = dao.readMember(id);
+			if (dto == null) {
+				System.out.println("검색 결과가 없습니다");
+				return;
+			}
+			printDTO(dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void findByName() {
+		printTitle("이름으로 회원 검색");
+		try {
+			System.out.print("이름 > ");
+			String name = br.readLine();
+			List<MemberDTO> list = dao.listMember(name);
+			if (list.size() == 0) {
+				System.out.println("검색 결과가 없습니다");
+				return;
+			}
+			System.out.println(list.size() + "개의 검색 결과가 있습니다.");
+			for (MemberDTO dto : list) {
+				printDTO(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void printDTO(MemberDTO dto) {
+		if (dto != null) {
+			System.out.print("[" + dto.getId() + ", ");
+			System.out.print(dto.getName() + ", ");
+			System.out.print(dto.getBirth() + ", ");
+			System.out.print(dto.getEmail() + ", ");
+			System.out.print(dto.getTel() + "]");
+			System.out.println();
+			return;
+		}
+		System.out.println("DTO is NULL... on printDTO(...)");
+	}
+}
